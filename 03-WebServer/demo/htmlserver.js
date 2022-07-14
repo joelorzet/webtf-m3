a60a3d0e350333b30dbe7f0a03cfe62b7bcb829c
joelorzet <joelorzt@gmail.com>
2022-07-13 21:35:54 -0300

    web server ready with node and nodemon

--- 03-WebServer/demo/htmlserver.js
+++ 03-WebServer/demo/htmlserver.js
@@ -1,11 +1,10 @@
 var http = require('http');
-var fs   = require('fs'); //Importamos el módulo fs que nos permite leer y escribir archivos del file system
+var fs = require('fs'); //Importamos el módulo fs que nos permite leer y escribir archivos del file system
 
-http.createServer( function(req, res){ 
-	
-	res.writeHead(200, { 'Content-Type':'text/html' })
-	var html = fs.readFileSync(__dirname +'/html/index.html');
-	res.end(html);
-
-
-}).listen(1337, '127.0.0.1');
+http
+	.createServer(function (req, res) {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		var html = fs.readFileSync(__dirname + '/html/index.html');
+		res.end(html);
+	})
+	.listen(1337, '127.0.0.1');
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,18 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require('fs');
+var http = require('http');
 
 // Escribí acá tu servidor
+
+http
+	.createServer((req, res) => {
+		fs.readFile(`${__dirname}/images/${req.url}.jpg`, (err, data) => {
+			if (err) {
+				res.writeHead(404, { 'Content-Type': 'text/plain' });
+				res.end('No existe el archivo que estas buscando');
+			} else {
+				res.writeHead(200, { 'Content-Type': 'image/jpg' });
+				res.end(data);
+			}
+		});
+	})
+	.listen(3001, 'localhost');
