ca122855bcd4b35cf9addd45706d85abec12cdb1
joelorzet <joelorzt@gmail.com>
2022-07-15 10:02:39 -0300

    primer parte de promesas listo

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -4,7 +4,37 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
+class $Promise {
+	constructor(executor) {
+		if (typeof executor !== 'function') throw new TypeError('executor needs to be a function');
 
+		this._state = 'pending';
+		this._value;
+
+		executor(
+			(val) => this._internalResolve(val),
+			(val) => this._internalReject(val)
+		);
+	}
+
+	_internalReject(err) {
+		if (this._state === 'pending' && this._state !== 'fulfilled') {
+			if (this._state === 'pending') {
+				this._value = err;
+			}
+			this._state = 'rejected';
+		}
+	}
+
+	_internalResolve(data) {
+		if (this._state === 'pending' && this._state !== 'rejected') {
+			if (this._state === 'pending') {
+				this._value = data;
+			}
+			this._state = 'fulfilled';
+		}
+	}
+}
 
 module.exports = $Promise;
 /*-------------------------------------------------------
