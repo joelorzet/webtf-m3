bce0082a29e7c215630e17d69556e695a4c10c62
joelorzet <joelorzt@gmail.com>
2022-07-21 12:17:05 -0300

    fizzBuzz listo generators functions

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,14 @@
-function fizzBuzzGenerator(max) {
-  // Tu código acá:
+function* fizzBuzzGenerator(max = Infinity) {
+	// Tu código acá:
+	let counter = 1;
 
+	while (counter <= max) {
+		if (counter % 3 === 0 && counter % 5 === 0) yield 'Fizz Buzz';
+		else if (counter % 3 === 0) yield 'Fizz';
+		else if (counter % 5 === 0) yield 'Buzz';
+		else yield counter;
+		counter++;
+	}
 }
 
 module.exports = fizzBuzzGenerator;
